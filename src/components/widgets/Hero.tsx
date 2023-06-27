bb45425380b2ae392ac6f309b2807d6aa494d496
ligdy <dy179912099@163.com>
2023-06-27 18:11:09 +0800

    style: width 优化

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,16 +1,16 @@
 import { component$ } from "@builder.io/qwik";
-import { Image } from "@unpic/qwik";
+// import { Image } from "@unpic/qwik";
 import styles from "./Hero.module.css";
 
 export default component$(() => {
   return (
     <section class={[styles.container, "dark:bg-none", "mt-[-65px]"]}>
       <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:flex h-screen pt-[72px]">
-        <div class="py-12 flex flex-row md:py-20 2xl:min-h-0 lg:flex lg:gap-8 lg:items-center lg:py-16">
-          <div class="mx-auto text-center basis-1/2 lg:text-left md:pb-16 pb-12 lg:pb-0">
+        <div class="py-12 flex flex-col md:flex-row md:py-20 2xl:min-h-0 lg:flex lg:gap-8 lg:items-center lg:py-16">
+          <div class="mx-auto text-center lg:text-left md:pb-16 pb-12 lg:pb-0">
             <>
-              <h1 class="text-5xl md:text-[3.48rem] font-bold leading-tighter tracking-tighter mb-4 font-heading px-4 md:px-0">
-                <span class="dark:text-[#039de1] font-mono">Free Soul</span>
+              <h1 class="text-3xl md:text-[3.48rem] font-bold leading-tighter tracking-tighter mb-4 font-heading px-4 md:px-0">
+                <span class="dark:text-[#039de1] font-mono">Hello World!</span>
               </h1>
               {/* <div class="max-w-3xl mx-auto">
                 <p class="text-xl text-gray-600 mb-8 dark:text-slate-400">
@@ -44,15 +44,15 @@ export default component$(() => {
               </div> */}
             </>
           </div>
-          <div class="basis-1/2">
+          <div>
             <div class="relative m-auto max-w-4xl">
-              <Image
+              {/* <Image
                 src="/images/bg.jpg"
                 class="mx-auto mt-4 mb-6 max-w-full rounded-3xl"
                 sizes="(max-width: 900px) 400px, 900px"
                 loading="eager"
                 width={400}
-              />
+              /> */}
             </div>
           </div>
         </div>
