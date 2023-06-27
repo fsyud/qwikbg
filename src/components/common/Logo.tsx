b748076939ab5131033946533bc1e8338706ffc1
ligdy <dy179912099@163.com>
2023-06-27 18:41:35 +0800

    style: add style

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => (
-  <span class="self-center ml-2 text-2xl md:text-xl text-gray-800 whitespace-nowrap dark:text-white flex items-center font-mon">
+  <span class="self-center text-2xl md:text-xl text-gray-800 whitespace-nowrap dark:text-white flex items-center font-mon">
     🤖
   </span>
 ));
--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -43,7 +43,7 @@ export default component$(() => {
 
   return (
     <footer class="border-t border-gray-200 dark:border-slate-800 ">
-      <div class="max-w-4xl mx-auto px-4 sm:px-6">
+      <div class="max-w-3xl mx-auto px-4 sm:px-6">
         <div class="grid grid-cols-12 gap-4 gap-y-8 sm:gap-8 py-8 md:py-12">
           <div class="col-span-12 lg:col-span-4 pr-8">
             <div class="mb-2">
