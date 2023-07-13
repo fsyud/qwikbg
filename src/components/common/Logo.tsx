9cc976e57feefffeacc250b9bbe1df1f5e39169e
ligdy <dy179912099@163.com>
2023-07-13 18:13:55 +0800

    feat: 构建

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => (
-  <span class="self-center text-2xl md:text-xl text-gray-800 whitespace-nowrap dark:text-white flex items-center font-mon">
-    ✏️
+  <span class="self-center md:text-base text-gray-800 whitespace-nowrap dark:text-white flex items-center">
+    Home
   </span>
 ));
